e13c75ec05a0a1bcf1060eb22ff05ff8525d40ec
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-04 18:32:15 +0530

    Update digits.c

--- digits.c
+++ digits.c
@@ -1,1 +1,23 @@
-
+/*counting the number of digits of a given number*/
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("Please enter the number:\n");
+  scanf("%d",&num);
+  if(num==0)
+  {
+    printf("The number entered is zero\n");
+  }
+  else
+  {
+    int count=0;
+    while(num>0)
+    {
+      num=num/10;
+      count++;
+    }
+    printf("The number of digits are/is\n",num);
+    system("pause>0");
+  }
+  
