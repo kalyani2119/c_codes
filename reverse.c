61c79ae2fee9fbe680f2f973c4a9c0faff674968
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-08 11:36:36 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int number,reverse=0;
+	printf("Please enter a number to be reversed\n");
+	scanf("%d",&number);
+	while (number!=0)
+	{
+		reverse = reverse * 10;
+		int lastnum = number % 10;
+		reverse = reverse + lastnum;
+		number=number/10;
+	}
+	printf("reversed number is %d\n",reverse);
+	return 0;
+}
